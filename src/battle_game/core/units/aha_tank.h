0a3ac20838efe3fabe8c9bd34fc8d68eeff3d8d2
Sockish <wlm24@mails.tsinghua.edu.cn>
2025-01-10 22:57:37 +0800

    aha

--- src/battle_game/core/units/aha_tank.h
+++ src/battle_game/core/units/aha_tank.h
@@ -2,9 +2,9 @@
 #include "battle_game/core/unit.h"
 
 namespace battle_game::unit {
-class Tank : public Unit {
+class ATank : public Unit {
  public:
-  Tank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  ATank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
   void Update() override;
   [[nodiscard]] bool IsHit(glm::vec2 position) const override;
@@ -21,4 +21,3 @@ class Tank : public Unit {
   uint32_t mine_count_down_{0};
 };
 }  // namespace battle_game::unit
-//cmake -S D:\THU\freshmen_autumn\"intro to programming"\battle_game\2024011338 -B D:\THU\freshmen_autumn\"intro to programming"\battle_game\2024011338\build -DCMAKE_TOOLCHAIN_FILE=D:\THU\vcpkg/scripts/buildsystems/vcpkg.cmake
